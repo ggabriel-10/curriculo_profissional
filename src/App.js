c0b025174cbac444194731cb2954efbcabdbee2e
Giovanni Medeiros <ggabriel.medeiros@hotmail.com>
2022-12-03 08:53:38 -0300

    Alteração de Navbar mobille

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Header } from './components/Header';
 import { Projetos } from './components/Projetos';
 import {Tecnologias} from './components/Tecnologias';
 import { Contato } from './components/Contato';
-import { useState } from 'react';
 
 function App() {
 
--- src/components/Conteudo.js
+++ src/components/Conteudo.js
@@ -11,7 +11,7 @@ export function Conteudo(){
                 <p>Meu nome é Giovanni Medeiros, tenho 22 anos, sou formado em Redes de Computadores pela Universidade Cidade de São Paulo e atualmente estou cursando Análise e Desenvolvimento de Sistemas na Universidade Estácio.</p>
                 <p>
                 Minha paixão por programação começou em 2020, quando eu comecei a estudar programação por conta própria. Desde então, venho estudando e aprimorando meus conhecimentos na área de desenvolvimento de software.
-                Com foco no desenvolvimento web, venho estudando e aprimorando meus conhecimentos em HTML, CSS, JavaScript, ReactJS e NodeJS. Atualmente estou estudando e aprimorando meus conhecimentos em React Native e TypeScript.
+                Com foco no desenvolvimento web, aprimorando meus conhecimentos em HTML, CSS, JavaScript, ReactJS e NodeJS. Atualmente estou estudando novas tecnologias como React Native e TypeScript.
                 </p>
 
             </div>
